d500950c7cc320243e3169da60f326d3b310aea3
James Dinsdale <james@superrb.com>
2020-03-19 11:56:18 +0000

    Update serverupdate.sh for Jekyll support

--- serverupdate.sh
+++ serverupdate.sh
@@ -8,6 +8,15 @@ else
   git pull origin master
 fi
 
+if [[ ! -f '_config/parameters.yml' ]]; then
+  cp _config/parameters.yml.dist _config/parameters.yml
+fi
+
+if [[ ! -f '.env' ]]; then
+  cp .env.dist .env
+fi
+
 # Run front end stuff
+bundle install --path vendor/bundle
 npm install
 gulp build
