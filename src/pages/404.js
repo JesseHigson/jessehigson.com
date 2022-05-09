1729964116f331abe2090e2eaf24a08b84de3757
Jesse Higson <jesse@superrb.com>
2022-05-09 20:10:31 +0100

    styling error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,15 +2,17 @@ import * as React from 'react'
 import { withPrismicUnpublishedPreview } from 'gatsby-plugin-prismic-previews'
 
 const NotFoundPage = () => (
-  <div className="not-found">
-    <h1>404</h1>
-    <h3>The page you are looking for was not found</h3>
-    <p>
-      <a href="/">
-        <button type="button">Return to homepage</button>
-      </a>
-    </p>
-  </div>
+  <section className="error">
+    <div className="error__container container">
+      <div className="error__content">
+        <h1 className="error__title">404</h1>
+        <h3 className="error__subtitle">The page you are looking for was not found</h3>
+        <a href="/" className="error__link">
+          Return to homepage
+        </a>
+      </div>
+    </div>
+  </section>
 )
 
 export default withPrismicUnpublishedPreview(NotFoundPage)
